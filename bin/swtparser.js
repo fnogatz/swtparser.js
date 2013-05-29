37af94aa6ee1b8bb5d1aec9b4d31f65eb536f957
Falco Nogatz <fnogatz@gmail.com>
2013-05-29 17:05:45 +0200

    added cli

---
+++ bin/swtparser.js
@@ -0,0 +1,24 @@
+var parser = require('../lib/index');
+
+var opts = require('nomnom')
+   .option('input', {
+      abbr: 'i',
+      flag: false,
+      help: 'SWT file',
+      required: true
+   })
+   .option('version', {
+      flag: true,
+      help: 'print version and exit',
+      callback: function() {
+         return require('../package.json').version;
+      }
+   })
+   .parse();
+
+parser.fromFile(opts.input, function(err, tnmt) {
+  if (err)
+    throw err;
+
+  console.log(tnmt);
+});
